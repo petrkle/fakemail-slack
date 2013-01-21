1d6c1c4f513a9c6a8d89de251773ff941889a7ab
Petr <petr@kle.cz>
2013-01-21 20:36:04 +0100

    Create home and run directory during install.

--- fakemail/install/doinst.sh
+++ fakemail/install/doinst.sh
@@ -5,4 +5,7 @@ USER=fakemail
 grep -q ${USER} /etc/group || echo "${USER}:x:300:" >> /etc/group
 grep -q ${USER} /etc/passwd || echo "${USER}:x:300:300:${USER}:/home/fakemail:/bin/false" >> /etc/passwd
 
+[ -d /home/${USER} ] || mkdir /home/${USER}
+[ -d /var/run/${USER} ] || mkdir /var/run/${USER}
+
 chown -R ${USER}:${USER} /home/${USER} /var/run/${USER}
